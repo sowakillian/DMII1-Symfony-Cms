466702a401b7f3367bce16b1796adef250e5f289
Sowa Killian <sowakillianpro@gmail.com>
2020-11-02 16:04:08 +0100

    validated twig added

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -12,7 +12,7 @@ class CommentController extends AbstractController
     /**
     * @Route("/form")
     */
-    public function new()
+    public function new(Request $request)
     {
         // creates a task object and initializes some data for this example
         $comment = new Comment();
@@ -21,8 +21,32 @@ class CommentController extends AbstractController
 
         $form = $this->createForm(CommentType::class, $comment);
 
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            $task = $form->getData();
+    
+            // ... perform some action, such as saving the task to the database
+            // for example, if Task is a Doctrine entity, save it!
+            // $entityManager = $this->getDoctrine()->getManager();
+            // $entityManager->persist($task);
+            // $entityManager->flush();
+    
+            return $this->redirectToRoute('comment_validated');
+        }
+
         return $this->render('comment/new.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+    /**
+    * @Route("/comment-validated", 
+    *        name="comment_validated")
+    */
+    public function validated() 
+    {
+        return $this->render('comment/validated.html.twig');
     }
 }
