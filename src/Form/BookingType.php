3b3d8b9d715918ff5f7f394f63f1e8624ea57ee7
Amélie <ameliemouillacpro@gmail.com>
2020-11-03 20:47:21 +0100

    add booking translation

--- src/Form/BookingType.php
+++ src/Form/BookingType.php
@@ -2,19 +2,52 @@
 
 namespace App\Form;
 
+use Symfony\Contracts\Translation\TranslatorInterface;
 use App\Entity\Booking;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class BookingType extends AbstractType
 {
+
+    private $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+    
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('loaningDate')
-            ->add('returnDate')
-            ->add('status')
+            ->add('loaningDate', DateTimeType::class, [
+                'label_translation_parameters' => [
+                    'Loaning date' => $this->translator->trans('Loaning date', [], 'booking'),
+                    'Year' => $this->translator->trans('Year', [], 'date'),
+                    'Month' => $this->translator->trans('Month', [], 'date'),
+                    'Day' => $this->translator->trans('Day', [], 'date'),
+                    'Hour' => $this->translator->trans('Hour', [], 'date'),
+                    'Minute' => $this->translator->trans('Minute', [], 'date'),
+                ],
+            ])
+            ->add('returnDate', DateTimeType::class, [
+                'label_translation_parameters' => [
+                    'Return date' => $this->translator->trans('Return date', [], 'booking'),
+                    'Year' => $this->translator->trans('Year', [], 'date'),
+                    'Month' => $this->translator->trans('Month', [], 'date'),
+                    'Day' => $this->translator->trans('Day', [], 'date'),
+                    'Hour' => $this->translator->trans('Hour', [], 'date'),
+                    'Minute' => $this->translator->trans('Minute', [], 'date'),
+                ],
+            ])
+            ->add('status', TextType::class, [
+                'label_translation_parameters' => [
+                    'Status' => $this->translator->trans('Status', [], 'booking'),
+                ],
+            ])
         ;
     }
 
