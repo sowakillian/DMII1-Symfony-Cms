8fbe32b679b5383b95aa969d33dbec6faec99038
Sowa Killian <sowakillianpro@gmail.com>
2020-11-15 11:22:37 +0100

    photos implemented in equipments

--- src/Controller/AdministrationController.php
+++ src/Controller/AdministrationController.php
@@ -3,6 +3,9 @@
 
 namespace App\Controller;
 
+use App\Repository\BookingRepository;
+use App\Repository\EquipementRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,8 +18,12 @@ class AdministrationController extends AbstractController
     /**
      * @Route("/", name="administration_index")
      */
-    public function index(): Response
+    public function index(UserRepository $userRepository, BookingRepository $bookingRepository, EquipementRepository $equipementRepository): Response
     {
-        return $this->render('administration/index.html.twig');
+        return $this->render('administration/index.html.twig', [
+            'users' => $userRepository->findAll(),
+            'bookings' => $bookingRepository->findAll(),
+            'equipments' => $equipementRepository->findAll(),
+        ]);
     }
 }
